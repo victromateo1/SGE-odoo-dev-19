f3199463b9d8b80cc61157a9540a6346c36ba245
VictorMateo <victormateo@gmail.com>
2025-02-21 09:43:14 +0100

    Update Demo Data

--- odoo/addons/vmn_aerolinea/__manifest__.py
+++ odoo/addons/vmn_aerolinea/__manifest__.py
@@ -23,7 +23,7 @@ Long description of module's purpose
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/menus.xml',
         'views/avion.xml',
         'views/azafata.xml',
